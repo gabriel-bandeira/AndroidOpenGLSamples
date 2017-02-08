663a799e1af92f259e662b292cd135df837bbeef
Gabriel Bandeira <gmb@cin.ufpe.br>
2017-02-08 15:58:50 -0300

    Check opengl version on AdrenoActivity

--- Cube/app/src/main/java/com/example/gmb/cube/AdrenoActivity.java
+++ Cube/app/src/main/java/com/example/gmb/cube/AdrenoActivity.java
@@ -1,13 +1,36 @@
 package com.example.gmb.cube;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import opengles.AdrenoGLRenderer;
+
 public class AdrenoActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adreno);
+
+        glSurfaceView = new GLSurfaceView(this);
+
+        final ActivityManager activityManager = (ActivityManager)
+                getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+        if (supportsEs2) {
+            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setRenderer(new AdrenoGLRenderer());
+        } else {
+            return;
+        }
+        setContentView(glSurfaceView);
+
     }
 }
